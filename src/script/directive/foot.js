8eea818f7202d536a01cae4eb4235db3ecd3df46
remmli <765745342@qq.com>
2018-03-03 16:39:16 +0800

    img

--- src/script/directive/foot.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('app').directive('appFoot', [function(){
-  return {
-    restrict: 'A',
-    replace: true,
-    templateUrl: 'view/template/foot.html'
-  }
-}]);
--- src/script/directive/head.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('app').directive('appHead',[function(){
-  return {
-    restrict: 'A',
-    replace: true,
-    templateUrl: 'view/template/head.html'
-  };
-}]);
